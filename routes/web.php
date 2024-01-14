f46ce481e03a5c60f3fd5e053f1587fb5a3f289d
chirtu <chitufatuma45@gmail.com>
2024-01-14 12:54:46 +0300

    add doctor page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +31,8 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+
+//make home route
+Route::get('/add_doctor_view',[AdminController::class,'addview']);
